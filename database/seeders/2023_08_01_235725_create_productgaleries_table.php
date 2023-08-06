81457517dbb6a5bfd87267233f0366e1bd545f6f
kodirzaelani <laman.kreasi@gmail.com>
2023-08-06 23:48:09 +0800

    update mogration product

---
+++ database/seeders/2023_08_01_235725_create_productgaleries_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('productgaleries', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->uuid('product_id');
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->string('tags')->nullable();
+            $table->float('price');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->primary('id');
+
+            $table->foreign('productcategory_id')->references('id')->on('productcategories')->cascadeOnDelete()->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productgaleries');
+    }
+};
